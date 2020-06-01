2a504d059750fae6e27f4a639b25768ba5cece68
Cyan_Heron <templar.of.silence@gmail.com>
2020-06-01 17:18:01 +0200

    Added rules layer

--- src/anfis_layers.py
+++ src/anfis_layers.py
@@ -81,11 +81,67 @@ class FuzzificationLayer(layers.Layer):
         # used Bell equation
         #   2/b instead of 2b for normalisation purpose
         # tf.repeat allows to expand input to fuzzy set count
-        return 1 / (1 + tf.pow(tf.abs((tf.repeat(inputs, self.fuzzy_sets_count) - self.c) / self.a), 2 / self.b))
+        return 1 / (1 + tf.pow(tf.abs((tf.repeat(inputs, self.fuzzy_sets_count, axis=1) - self.c) / self.a), 2 / self.b))
 
     def get_config(self):
         # for serialisation only, in case we want to save model directly
         base_config = super(FuzzificationLayer, self).get_config()
+        base_config['fuzzy_sets_count'] = self.fuzzy_sets_count
+        base_config['output_dim'] = self.units
+        return base_config
+
+
+class RulesLayer(layers.Layer):
+    # TODO : ADD APRIORI RULES
+    def __init__(self, fuzzy_sets_count, **kwargs):
+        """Rules layer for ANFIS.
+
+          Args:
+            fuzzy_sets_count: a list of the number of fuzzy sets for each input parameter
+
+            Example:
+                 for the following set labels [['warm','normal','cold'],['dark','bright'],['very slow','slow','fast','very fast']]
+                 fuzzy_sets_count = [3,2,4]
+                 (3 for temperature, 2 for lightness and 4 for speed)
+
+        """
+        super(RulesLayer, self).__init__(**kwargs)
+        self.fuzzy_sets_count = fuzzy_sets_count
+
+        if self.fuzzy_sets_count is None:
+            raise Exception('FuzzificationLayer', '\'units_dimensions\' can\'t be None.')
+        if len(self.fuzzy_sets_count) < 1:
+            raise Exception('FuzzificationLayer', '\'units_dimensions\' must have at least one element.')
+
+        self.units = None
+
+    def build(self, input_shape):
+        # tensorflow reported bug troubleshooting  https://stackoverflow.com/questions/56094714/how-can-i-call-a-custom-layer-in-keras-with-the-functional-api
+        input_shape = input_shape.as_list()
+
+        if input_shape[1] < 1:
+            raise Exception('FuzzificationLayer', '\'input_size\' must be at least 1.')
+        len_ud = len(self.fuzzy_sets_count)
+        if input_shape[1] > len_ud:
+            self.units = np.prod(self.fuzzy_sets_count) * self.fuzzy_sets_count[len_ud - 1] ** (input_shape[1] - len_ud)
+        elif input_shape[1] == len_ud:
+            self.units = np.prod(self.fuzzy_sets_count)
+        else:
+            raise Exception('FuzzificationLayer',
+                            '\'units_dimensions\' have more elements than there are input parameters.')
+
+    def call(self, inputs, **kwargs):
+        split = tf.split(inputs, self.fuzzy_sets_count, axis=1)
+        res = tf.expand_dims(split[0], 2)
+        for i in range(1, len(split)):
+            res = tf.matmul(res, tf.expand_dims(split[i], 2), transpose_b=True)
+            print(res.shape)
+            res = tf.reshape(res, shape=[-1, res.shape[1]*res.shape[2], 1])
+        return tf.reshape(res, shape=[-1, res.shape[1]])
+
+    def get_config(self):
+        # for serialisation only, in case we want to save model directly
+        base_config = super(RulesLayer, self).get_config()
         base_config['fuzzy_sets_count'] = self.fuzzy_sets_count
         base_config['output_dim'] = self.units
         return base_config
--- src/debug.py
+++ src/debug.py
@@ -1,5 +1,5 @@
 from tensorflow.keras.layers import Input
-from anfis_layers.fuzzification_layer import FuzzificationLayer
+from anfis_layers import FuzzificationLayer, RulesLayer
 import tensorflow as tf
 
 # reset console, just in case
@@ -10,6 +10,9 @@ f0 = Input(shape=(3,))
 
 # fuzzification layer predator falls in 3 categories, prey in 4 categories, food in 3
 #   values are random TODO DEBUG ONLY
-fl = FuzzificationLayer(fuzzy_sets_count=[3, 4, 3])(f0)
+fuzzy_sets_count = [3, 4, 3]
+f1 = FuzzificationLayer(fuzzy_sets_count=fuzzy_sets_count)(f0)
 
+# rules layer as before
+f2 = RulesLayer(fuzzy_sets_count=fuzzy_sets_count)(f1)
 a = 1
